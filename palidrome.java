1e17f3e41695210c7f6e01e0963e06ed3c71f5d7
Thi Hong Ha Vu <vhh0102@gmail.com>
2019-05-09 16:31:42 -0500

    Create palidrome.java

---
+++ palidrome.java
@@ -0,0 +1,23 @@
+# Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+# My solution
+class Solution {
+    public boolean isPalindrome(int x) {
+        String number = String.valueOf(x);
+        char[] digits = number.toCharArray();
+        String[] arr = new String[digits.length];
+        for (int i = digits.length - 1; i >= 0; i--)
+        {
+            arr[digits.length - 1 - i] = String.valueOf(digits[i]);
+        }
+            
+        String str = "";
+        for (int j = 0; j < digits.length; j++)
+        {
+            str = str + arr[j];
+        }
+        
+
+        return str.equals(number);
+        }
+}
