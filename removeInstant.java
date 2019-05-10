f8a82816a2e897a3d8f9711159dab6cce2e8adee
Thi Hong Ha Vu <vhh0102@gmail.com>
2019-05-09 19:26:00 -0500

    Create removeInstant.java

---
+++ removeInstant.java
@@ -0,0 +1,67 @@
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+// Super dumb and long solution, and Idk why leetcode doesn't accept this for case arr = [0] and val = 2 for example.
+// Worked in Eclipse though.
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    if (nums.length == 0) {
+        return nums.length;
+    }
+    else if (nums.length == 1 && val == nums[0]) {
+        nums = new int[0];
+        return nums.length;
+    }
+    else if (nums.length == 1 && val != nums[0]) {
+        return 0;
+    }
+    else {
+    ArrayList<Integer> index = new ArrayList<>();
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            index.add(i);
+        }
+    }
+    
+    int j = 0;
+    if (index.size() == 0) {
+    	nums = new int[0];
+        return nums.length;
+    }
+    else if (index.size() == 1) {
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] == val) {
+                nums[k] = nums[index.get(0)];
+                break;
+            }
+        }
+    }
+    else {
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] != val && j < index.size()) {
+                nums[k] = nums[index.get(j)];
+                j++;
+            }
+            if (nums[k] == val && j < index.size()) {
+                nums[k] = nums[index.get(j)];
+                j++;
+            }
+        }
+    }
+
+    return j;
+    }
+    }
+}
+
+// Their solution
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
